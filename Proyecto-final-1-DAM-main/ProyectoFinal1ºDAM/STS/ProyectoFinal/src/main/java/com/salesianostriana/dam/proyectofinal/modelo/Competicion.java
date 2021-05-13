a56b3f0c01184426d9834d59bdfc5d55f594f90d
Alfonsogallardo7 <74561560+Alfonsogallardo7@users.noreply.github.com>
2021-05-13 14:23:44 +0200

    Arreglando carrusel y mejorando menu

---
+++ Proyecto-final-1-DAM-main/ProyectoFinal1ºDAM/STS/ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/modelo/Competicion.java
@@ -0,0 +1,34 @@
+package com.salesianostriana.dam.proyectofinal.modelo;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Competicion {
+	
+	@Id
+	@GeneratedValue
+	
+	private long id;
+	private String nombreCompeticion;
+	private String nombreClubPromotor;
+	private String direccion;
+	private int codigoPostal;
+	private String provincia;
+	private LocalDate fechaCompeticion;
+	
+	//No se si es booleano o no por que seria un menú desplegable
+	private String categoriaCompeticion;
+	
+
+}
---
+++ Proyecto-final-1-DAM-main/ProyectoFinal1ºDAM/STS/ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/modelo/Competidor.java
@@ -0,0 +1,49 @@
+package com.salesianostriana.dam.proyectofinal.modelo;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+public class Competidor {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private String nombre;
+	private String apellidos;
+	private LocalDate fechaNacimiento;
+	private double pesoActual;
+	//No se si seria booleano o no por que sería de un menu desplegable
+	private int categoriaPeso;
+	//Igual
+	private String sexo;
+	private double squat;
+	private double benchPress;
+	private double deadlift;
+	private double total;
+
+	public Competidor(String nombre, String apellidos, LocalDate fechaNacimiento, double pesoActual, int categoriaPeso,
+			String sexo, double squat, double benchPress, double deadlift) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+		this.pesoActual = pesoActual;
+		this.categoriaPeso = categoriaPeso;
+		this.sexo = sexo;
+		this.squat = squat;
+		this.benchPress = benchPress;
+		this.deadlift = deadlift;
+	}
+
+}
