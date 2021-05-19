580db1f7789594921d3e7bd1576585eef811dedf
Alfonsogallardo7 <74561560+Alfonsogallardo7@users.noreply.github.com>
2021-05-19 14:24:23 +0200

    Delete ProyectoFinal1ºDAM directory

--- ProyectoFinal1ºDAM/STS/ProyectoFinal/src/main/java/com/salesianostriana/dam/proyectofinal/repository/CompetidorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.proyectofinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.proyectofinal.modelo.Competidor;
-
-public interface CompetidorRepository extends JpaRepository<Competidor, Long> {
-
-}
